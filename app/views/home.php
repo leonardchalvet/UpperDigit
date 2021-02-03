3e12dcb6020592f92304a45d83bb0516ce8e9af8
Alexis Prevost-Maynen <killer76520@gmail.com>
2021-02-03 11:39:20 +0100

    Change - Back - Home - Include "common section start"

--- app/views/home.php
+++ app/views/home.php
@@ -227,22 +227,7 @@ $document = $WPGLOBAL['document']->data;
         </div>
       </section>
 
-      <section class="section-start">
-        <div class="wrapper">
-          <div class="container-text">
-            <?= RichText::asHtml($document->start_title); ?>
-            <?= RichText::asHtml($document->start_text); ?>
-          </div>
-          <div class="container-btn">
-            <a class="btn" href="<?= checkUrl($document->start_btnlink); ?>">
-              <div class="btn-text"><?= RichText::asText($document->start_btntext); ?></div>
-              <svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 16" >
-                <use xlink:href="/img/common/icn-arrow.svg#content"></use>
-              </svg>
-            </a>
-          </div>
-        </div>
-      </section>
+      <?php include('common-section-start.php') ?>
 
     </main>
 

d03e093e0bc2da4a619e1a3031003ebe0df232b9
Alexis Prevost-Maynen <killer76520@gmail.com>
2021-02-03 11:38:42 +0100

    Change - Back - Link - Prismic

--- app/views/common-header.php
+++ app/views/common-header.php
@@ -5,21 +5,20 @@ $header = $WPGLOBAL['header']->data;
 
 <header>
 	<div class="wrapper">
-		<a href="" class="logo">
-			<img src="/img/common/logo.svg" alt="">
+		<a href="<?= checkUrl($header->logo_link); ?>" class="logo">
+			<img src="<?= $header->logo_img->url; ?>" alt="<?= $footer->logo_img->alt; ?>">
 		</a>
-
 		<div class="container-action">
-			<a href="" class="signin">
+			<a href="<?= checkUrl($header->cta_btnlink1); ?>" class="signin">
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" >
 					<use xlink:href="/img/header/icn-user.svg#content"></use>
 				</svg>
 				<span class="text">
-					<span class="link-text">Se connecter</span>
+					<span class="link-text"><?= RichText::asText($header->cta_btntext1); ?></span>
 				</span>
 			</a>
-			<a href="" class="start">
-				<div class="btn-text">S’abonner</div>
+			<a href="<?= checkUrl($header->cta_btnlink2); ?>" class="start">
+				<div class="btn-text"><?= RichText::asText($header->cta_btntext2); ?></div>
 				<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 16" >
 					<use xlink:href="/img/common/icn-arrow.svg#content"></use>
 				</svg>

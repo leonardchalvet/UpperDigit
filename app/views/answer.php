ed48040aebd1b8bd9cc1c9251a9eb5cdc8ab7f76
Alexis Prevost-Maynen <killer76520@gmail.com>
2021-02-09 12:05:47 +0100

    Change - Back - Answer - pre-link

---
+++ app/views/answer.php
@@ -0,0 +1,206 @@
+<?php 
+use Prismic\Dom\RichText;
+$document = $WPGLOBAL['document']->data;
+?>
+<html>
+  <head>
+
+    <title><?= RichText::asText($document->global_title); ?></title>
+
+    <meta name="description" content="<?= RichText::asText($document->global_description); ?>" />
+
+    <meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="/style/css/answer.css">
+
+  </head>
+  
+  <body>
+
+    <?php include('common-header.php') ?>
+
+    <main>
+      
+      <div class="container-lightbox">
+        <div class="lightbox lightbox-1">
+          <div class="cross">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" >
+              <use xlink:href="img/common/icn-cross.svg#content"></use>
+            </svg>
+          </div>
+          <h3>Ces informations vous ont-elles étés utiles ?</h3>
+          <p class="desc">
+            Dans l’optique d’améliorer notre service et nos réponses, veuillez répondre à notre enquête de satifaction afin d’améliorer no
+          </p>
+          <div class="container-action">
+            <div class="btn">
+              <div class="btn-text">Oui</div>
+            </div>
+            <div class="btn">
+              <div class="btn-text">Non</div>
+            </div>
+          </div>
+          <p class="text">
+            Merci de votre réponse et de votre visite. À bientôt !
+          </p>
+        </div>
+        <div class="lightbox lightbox-2">
+          <div class="cross">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" >
+              <use xlink:href="img/common/icn-cross.svg#content"></use>
+            </svg>
+          </div>
+          <h3>Merci de votre confiance !</h3>
+          <p class="desc">
+            Vous avez atteint le nombre de recherches gratuites.<br>
+            <br>
+            Pour continuer l'expérience, nous vous invitons à vous créer un compte afin de pouvoir continuer à poser vos questions 
+          </p>
+          <div class="container-btn">
+            <div class="btn">
+              <div class="btn-text">Abonnez-vous dès maintenant</div>
+              <svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 16" >
+                <use xlink:href="img/common/icn-arrow.svg#content"></use>
+              </svg>
+            </div>
+          </div>
+          <p class="text">
+            Vous êtes déjà inscrit ? <a href="">Connectez-vous</a>
+          </p>
+        </div>
+      </div>
+
+      <section class="section-search">
+        <div class="wrapper">
+          <div class="container-search">
+            <div class="container-input">
+              <div class="input">
+                <div class="search">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" >
+                    <use xlink:href="img/common/icn-search.svg#content"></use>
+                  </svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" >
+                    <use xlink:href="img/common/icn-search.svg#content"></use>
+                  </svg>
+                </div>
+                <input type="text" placeholder="Posez une autre question">
+                <div class="container-action">
+                  <button>
+                    <svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 16" >
+                      <use xlink:href="img/common/icn-arrow.svg#content"></use>
+                    </svg>
+                  </button>
+                  <div class="cross">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" >
+                      <use xlink:href="img/common/icn-cross.svg#content"></use>
+                    </svg>
+                  </div>
+                </div>
+              </div>
+              <div class="dropdown">
+                <div class="container-placeholder">
+                  <ul>
+                    <li>
+                      <div class="title">Questions récurentes</div>
+                    </li>
+                    <li>
+                      <a href="">Comment faire sa comptabilité ?</a>
+                    </li>
+                    <li>
+                      <a href="">Comment faire sa comptabilité ?</a>
+                    </li>
+                    <li>
+                      <a href="">Comment faire sa comptabilité ?</a>
+                    </li>
+                    <li>
+                      <a href="">Comment faire sa comptabilité ?</a>
+                    </li>
+                  </ul>
+                </div>
+                <div class="container-result">
+                  <ul>
+                    <li>
+                      <a href="">Comment faire sa comptabilité ?</a>
+                    </li>
+                    <li>
+                      <a href="">Comment faire sa comptabilité ?</a>
+                    </li>
+                    <li>
+                      <a href="">Comment faire sa comptabilité ?</a>
+                    </li>
+                    <li>
+                      <a href="">Comment faire sa comptabilité ?</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>  
+      </section>
+
+      <section class="section-answer">
+        <div class="wrapper">
+          <div class="container-answer">
+            <div class="answer">
+              <h2>Ou peut-on trouver le numéro intracommunautaire ?</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo eaque, placeat, neque facere consequatur ipsum et id, dolorem necessitatibus hic, dolore minus tenetur commodi eveniet in corrupti dolorum adipisci aut?
+              </p>
+              <p class="blur-answer">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in ornare quam viverra orci sagittis eu volutpat odio facilisis mauris sit amet massa vitae tortor condimentum lacinia quis vel eros donec ac odio tempor orci dapibus ultrices in iaculis nunc sed augue lacus, viverra vitae congue eu, consequat ac felis donec et odio pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus
+              </p>
+            </div>
+            <div class="container-informations">
+              <h3>Ces informations vous ont-elles étés utiles ?</h3>
+              <div class="container-action">
+                <div class="btn">
+                  <div class="btn-text">Oui</div>
+                </div>
+                <div class="btn">
+                  <div class="btn-text">Non</div>
+                </div>
+              </div>
+            </div>
+            <div class="resp">
+              <h4>Disclaimer sur la responsabilité</h4>
+              <p>
+                Phasellus quis orci nunc. Sed ut ullamcorper tortor. Cras laoreet aliquet nisi, at vehicula turpis consectetur sit amet. Sed ut sapien ipsum. Curabitur sagittis lobortis nulla, ut ultrices justo mollis sed. Donec vitae ullamcorper nisl, ut varius nulla. Nullam quam ipsum, tempor ac sodales vitae, accumsan tincidunt augue. 
+              </p>
+            </div>
+          </div>
+          <div class="container-questions">
+            <h2>Questions similaires</h2>
+            <ul>
+              <li>
+                <a href="">
+                  <span class="link-text">Quel est le but de la comptabilité ?</span>
+                  
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <span class="link-text">Quel est le but de la comptabilité ?</span>
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <span class="link-text">Quel est le but de la comptabilité ?</span>
+                </a>
+              </li>
+            </ul>
+          </div>  
+        </div>
+      </section>
+
+      <?php include('common-section-start.php') ?>
+
+    </main>
+
+    <?php include('common-footer.php') ?>
+
+    <script type="text/javascript" src="/script/minify/answer-min.js"></script>
+  </body>
+</html>

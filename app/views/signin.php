c8e07816f5cba6bf3b81199f8b2417ecc149ac73
Alexis Prevost-Maynen <killer76520@gmail.com>
2021-02-19 09:08:45 +0100

    Change - Back - Signin - Link Prismic

---
+++ app/views/signin.php
@@ -0,0 +1,67 @@
+<?php 
+use Prismic\Dom\RichText;
+$document = $WPGLOBAL['document']->data;
+?>
+<html>
+  <head>
+
+    <title><?= RichText::asText($document->global_title); ?></title>
+
+    <meta name="description" content="<?= RichText::asText($document->global_description); ?>" />
+
+    <meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="/style/css/signin.css">
+
+  </head>
+  
+  <body>
+
+    <?php include('common-header.php') ?>
+
+    <main>
+      
+      <section class="section-signin">
+        <div class="wrapper">
+          <div class="container-illu">
+            <img src="<?= $document->content_img->url; ?>" alt="<?= $document->content_img->alt; ?>">
+          </div>
+          <div class="container-form">
+            <?= RichText::asHtml($document->content_title); ?>
+            <?= RichText::asHtml($document->content_text); ?>
+            <form onSubmit="return false;">
+              <div class="input">
+                <input type="email" placeholder="<?= RichText::asText($document->content_email); ?>">
+                <div class="error"><?= RichText::asText($document->content_error); ?></div>
+              </div>
+              <div class="input">
+                <input type="password" placeholder="<?= RichText::asText($document->content_password); ?>">
+                <div class="error"><?= RichText::asText($document->content_error); ?></div>
+              </div>
+              <div class="password-forg">
+                <a href="<?= checkUrl($document->content_forgetlink); ?>"><?= RichText::asText($document->content_forgettext); ?></a>
+              </div>
+              <div class="container-btn">
+                <button>
+                  <div class="btn-text">
+                    <?= RichText::asText($document->content_btntext); ?>
+                  </div>
+                </button>
+              </div>
+              <div class="signup">
+                <?= RichText::asHtml($document->content_subscribe); ?>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+
+    </main>
+
+    <?php include('common-footer.php') ?>
+
+    <script type="text/javascript" src="/script/minify/signin-min.js"></script>
+  </body>
+</html>

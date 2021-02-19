d335296462759c5d9ce7e23618bc2017efba318d
Alexis Prevost-Maynen <killer76520@gmail.com>
2021-02-19 09:06:57 +0100

    Change - Front - Common - add common function

--- Front/script/js/common.js
+++ Front/script/js/common.js
@@ -1,1 +1,10 @@
 // @codekit-prepend 'jQuery.3.3.1.js'
+
+function isEmpty(el){
+	return !$.trim(el.val());
+}
+
+function verifEmail(c) {
+	let regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+	return regex.test(c.val());
+}

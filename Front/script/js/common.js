a8e0a518ff5b8515d0d40e3f775e315021273d38
Alexis Prevost-Maynen <alexis@callbruno.com>
2021-03-04 11:48:21 +0100

    Change - Front - Common - Add cookie

--- Front/script/js/common.js
+++ Front/script/js/common.js
@@ -8,3 +8,43 @@ function verifEmail(c) {
 	let regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
 	return regex.test(c.val());
 }
+
+function getCookie(cname) {
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(';');
+	for(let i = 0; i <ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function setCookie(cname, cvalue, exdays) {
+	let d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	let expires = "expires="+ d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function checkCookie() {
+	if(!getCookie('cookie')) {
+
+		$('.container-cookies').addClass('style-show');
+
+		$('.container-cookies .btn:nth-child(1)').click(function(){
+			$('.container-cookies').removeClass('style-show');
+		});
+
+		$('.container-cookies .btn:nth-child(2)').click(function(){
+			$('.container-cookies').removeClass('style-show');
+			setCookie('cookie');
+		});
+	}
+}
+checkCookie();

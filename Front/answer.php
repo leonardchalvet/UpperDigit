b4f2787f26546ce7f408c8ef4526d1f5bcde8d98
Alexis Prevost-Maynen <alexis@callbruno.com>
2021-03-04 11:47:07 +0100

    Change - Front - Answer - Link width algolia & Fix css

--- Front/answer.php
+++ Front/answer.php
@@ -11,6 +11,8 @@
 
 		<link rel="stylesheet" type="text/css" href="style/css/answer.css">
 
+		<script src="https://cdn.jsdelivr.net/npm/algoliasearch@4.5.1/dist/algoliasearch-lite.umd.js"></script>
+
 	</head>
 	
 	<body>
--- Front/script/js/answer.js
+++ Front/script/js/answer.js
@@ -1,46 +1,99 @@
 // @codekit-prepend 'common.js'
 
-$('.section-search .container-search .container-input .input input').focusin(function(){
-	$('.section-search .container-search .container-input .input').addClass('style-search');
-	$('.section-search .container-search .container-input .dropdown').addClass('style-show');
-	if (!$('.section-search .container-search .container-result').hasClass('style-show')) {
-		$('.section-search .container-search .container-placeholder').addClass('style-show');
-	}
-})
+$(window).on('load', function() {
 
-$('.section-search .container-search .container-input .input input').focusout(function(){
-	if ($(this).val() === "") {
+	$('.section-search .container-search .container-input .input input').focusout(function(){
+		if ($(this).val() === "") {
+			$('.section-search .container-search .container-input .input').removeClass('style-search');
+			$('.section-search .container-search .container-input .dropdown').removeClass('style-show');
+		}
+	})
+
+	$('.section-search .container-search .container-input .input .container-action button').click(function(){
+		$('.section-search .container-search .container-input .input input').focus();
+	})
+
+	function closeSearch() {
 		$('.section-search .container-search .container-input .input').removeClass('style-search');
 		$('.section-search .container-search .container-input .dropdown').removeClass('style-show');
-	};
-})
 
-$('.section-search .container-search .container-input .input .container-action button').click(function(){
-	$('.section-search .container-search .container-input .input input').focus();
-})
-
-$('.section-search .container-search .container-input .input .container-action .cross').click(function(){
-	$('.section-search .container-search .container-input .input input').val('');
-	$('.section-search .container-search .container-input .input').removeClass('style-search');
-	$('.section-search .container-search .container-input .dropdown').removeClass('style-show');
-
-	$('.section-search .container-search .container-result').removeClass('style-show');
-	$('.section-search .container-search .container-placeholder').addClass('style-show');
-})
-
-$('.section-search .container-search .container-input .input input').on('keyup', function() {
-	if (this.value.length > 1) {
-		$('.section-search .container-search .container-placeholder').removeClass('style-show');
-		$('.section-search .container-search .container-result').addClass('style-show');
-
-	} else {
 		$('.section-search .container-search .container-result').removeClass('style-show');
 		$('.section-search .container-search .container-placeholder').addClass('style-show');
 	}
+
+	$('.section-search .container-search .container-input .input .container-action .cross').click(function(){
+		$('.section-search .container-search .container-input .input input').val('');
+		closeSearch();
+	})
+
+	/* ALGOLIA */
+	const client = algoliasearch('XPXU1DOA36', 'dbfeccb756f9c49a7d852aece7784c44');
+	const index = client.initIndex('question_reponse');
+
+	function resultPlaceholder() {
+		index.search(" ", { highlightPreTag: '<em>', hitsPerPage: 4 }).then(({ hits }) => {
+			let arrayLi = '<li><div class="title">' + titlePlaceholder + "</div></li>";
+			$(hits).each(function(index) {
+				arrayLi += '<li><div class="answer" data-answer="' + hits[index]._highlightResult.reponse.value + '">' + hits[index]._highlightResult.question.value + "</div></li>";
+			});
+			$('.section-search .container-search .container-input .dropdown .container-placeholder ul li').remove();
+			$('.section-search .container-search .container-input .dropdown .container-placeholder ul').append(arrayLi);
+			updateClickAnswer();
+		});
+	}
+
+	let titlePlaceholder = $('.section-search .container-search .container-placeholder .title').text();
+	$(".section-search .container-search .container-input .input input").on("keyup", function() {
+		let value = $(this).val().toLowerCase();
+		if(value.length > 0) {
+
+			$('.section-search .container-search .container-input .input').addClass('style-search');
+			$('.section-search .container-search .container-input .dropdown').addClass('style-show');
+			$('.section-search .container-search .container-placeholder').removeClass('style-show');
+			$('.section-search .container-search .container-result').addClass('style-show');
+
+			index.search(value, { highlightPreTag: '<em>', hitsPerPage: 4 }).then(({ hits }) => {
+				let arrayLi = "";
+				$(hits).each(function(index) {
+					arrayLi += '<li><div class="answer" data-answer="' + hits[index]._highlightResult.reponse.value + '">' + hits[index]._highlightResult.question.value + "</div></li>";
+				});
+				$('.section-search .container-search .container-input .dropdown .container-result ul li').remove();
+				$('.section-search .container-search .container-input .dropdown .container-result ul').append(arrayLi);
+				updateClickAnswer();
+			});
+		}
+		else {
+			$('.section-search .container-search .container-result').removeClass('style-show');
+			$('.section-search .container-search .container-placeholder').addClass('style-show');
+
+			resultPlaceholder();
+		}
+	});
+
+	$('.section-search .container-search .container-input .input input').focusin(function(){
+		$('.section-search .container-search .container-input .input').addClass('style-search');
+		$('.section-search .container-search .container-input .dropdown').addClass('style-show');
+		
+		if (!$('.section-search .container-search .container-result').hasClass('style-show')) {
+			$('.section-search .container-search .container-placeholder').addClass('style-show');
+		}
+
+		resultPlaceholder();
+	});
+
+	function updateClickAnswer() {
+		$('.section-search .container-search .container-input .dropdown ul li .answer').click(function(){
+			$('.section-search .container-search .container-input .input input[name="question"]').val($(this).text());
+			$('.section-search .container-search .container-input .input input[name="answer"]').val($(this).attr('data-answer'));
+
+			$('.section-search form button').click();
+		});
+	}
+
+	$('.container-lightbox .lightbox .cross').click(function(){
+		$('.container-lightbox .lightbox').removeClass('style-show');
+		$('.container-lightbox').removeClass('style-show');
+	})
+
+	/* END ALGOLIA */
 });
-
-
-$('.container-lightbox .lightbox .cross').click(function(){
-	$('.container-lightbox .lightbox').removeClass('style-show');
-	$('.container-lightbox').removeClass('style-show');
-})

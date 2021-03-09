30a152244e1a3b787d4b092960e0c7b06996c4ee
Alexis Prevost-Maynen <alexis@callbruno.com>
2021-03-09 16:30:34 +0100

    UPDATE FRONT -> BACK

--- public/script/js/common.js
+++ public/script/js/common.js
@@ -25,6 +25,22 @@ function getCookie(cname) {
 	return false;
 }
 
+function getCookieAndValue(cname) {
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(';');
+	for(let i = 0; i <ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.split('=');
+		}
+	}
+	return false;
+}
+
 function setCookie(cname, cvalue, exdays) {
 	let d = new Date();
 	d.setTime(d.getTime() + (exdays*24*60*60*1000));

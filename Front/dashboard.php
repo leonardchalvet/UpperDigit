6196f5cf6539a0a7ceb1c0509a9bf67672073250
Léonard Chalvet <leonard@agence-me.com>
2021-03-12 09:20:04 +0100

    Add - Front - Dashboard

---
+++ Front/dashboard.php
@@ -0,0 +1,408 @@
+<html>
+	<head>
+
+		<title>Title</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/css/dashboard.css">
+
+	</head>
+	
+	<body>
+
+
+		<?php include('common-header.php') ?>
+
+		
+
+		<main>
+			
+			<section class="section-dashboard">
+				<div class="wrapper">
+					<div class="container-sidebar">
+						<div class="title">Paramètres</div>
+						<ul class="container-tab">
+							<li class="tab style-active">
+								<span>Mes Informations</span>
+							</li>
+							<li class="tab">
+								<span>Mes Factures</span>
+							</li>
+						</ul>
+					</div>
+					<div class="container-informations">
+						<section class="section-informations">
+							<div class="head">
+								<div class="title">Mes informations</div>
+								<div class="container-btn">
+									<div class="btn">
+										<div class="btn-text">Editer</div>
+									</div>
+									<div class="btn">
+										<div class="btn-text">Enregistrer</div>
+									</div>
+								</div>
+							</div>
+							<div class="container-content">
+								<div class="content-save">
+									<div class="container-el">
+										<div class="el">
+											<ul>
+												<li>
+													<div class="title">
+														Nom
+													</div>
+
+													<div class="text">
+														Carvalho
+													</div>
+												</li>
+												<li>
+													<div class="title">
+														Prénom
+													</div>
+
+													<div class="text">
+														Claudia
+													</div>
+												</li>
+												<li>
+													<div class="title">
+														Téléphone
+													</div>
+
+													<div class="text">
+														(907) 555-0101
+													</div>
+												</li>
+												<li>
+													<div class="title">
+														Adresse
+													</div>
+
+													<div class="text">
+														2464 Royal Ln. Mesa
+													</div>
+												</li>
+												<li>
+													<div class="title">
+														Code Postal
+													</div>
+
+													<div class="text">
+														45463
+													</div>
+												</li>
+												<li>
+													<div class="title">
+														Pays
+													</div>
+
+													<div class="text">
+														United States
+													</div>
+												</li>
+											</ul>
+										</div>
+										<div class="el">
+											<ul>
+												<li>
+													<div class="title">
+														Email
+													</div>
+
+													<div class="text">
+														jessica.hanson@example.com
+													</div>
+												</li>
+												<li>
+													<div class="title">
+														Mot de passe
+													</div>
+
+													<div class="text">
+														********
+													</div>
+												</li>
+											</ul>
+										</div>
+										<div class="el">
+											<ul>
+												<li>
+													<div class="title">
+														Titulaire
+													</div>
+
+													<div class="text">
+														Claudia Carvalho
+													</div>
+												</li>
+												<li>
+													<div class="title">
+														Numéro de carte
+													</div>
+
+													<div class="text">
+														VISA   ****  ****  ****  6589
+													</div>
+												</li>
+											</ul>
+										</div>
+										<div class="el">
+											<ul>
+												<li>
+													<div class="title">
+														Société
+													</div>
+
+													<div class="text">
+														Sony
+													</div>
+												</li>
+												<li>
+													<div class="title">
+														SIRET
+													</div>
+
+													<div class="text">
+														453
+													</div>
+												</li>
+												<li>
+													<div class="title">
+														Siège Social
+													</div>
+
+													<div class="text">
+														Lorem
+													</div>
+												</li>
+												<li>
+													<div class="title">
+														Adresse
+													</div>
+
+													<div class="text">
+														2464 Royal Ln. Mesa
+													</div>
+												</li>
+												<li>
+													<div class="title">
+														Code Postal
+													</div>
+
+													<div class="text">
+														45463
+													</div>
+												</li>
+												<li>
+													<div class="title">
+														Pays
+													</div>
+
+													<div class="text">
+														United States
+													</div>
+												</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+								<div class="content-edit">
+									<div class="container-el">
+										<div class="el" data-info="perso">
+											<div class="title">Informations personnelles</div>
+											<div class="container-input">
+												<div class="wrap-input">
+													<div class="input">
+														<input required type="text" placeholder="Prénom*">
+														<div class="error">Error</div>
+													</div>
+												</div>
+												<div class="wrap-input">
+													<div class="input">
+														<input required type="text" placeholder="Prénom*">
+														<div class="error">Error</div>
+													</div>
+												</div>
+												<div class="wrap-input">
+													<div class="input">
+														<input required type="text" placeholder="Prénom*">
+														<div class="error">Error</div>
+													</div>
+												</div>
+												<div class="wrap-input">
+													<div class="input">
+														<input required type="text" placeholder="Prénom*">
+														<div class="error">Error</div>
+													</div>
+												</div>
+												<div class="wrap-input">
+													<div class="input">
+														<input required type="text" placeholder="Prénom*">
+														<div class="error">Error</div>
+													</div>
+													<div class="input">
+														<input required type="text" placeholder="Prénom*">
+														<div class="error">Error</div>
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="el" data-info="securite">
+											<div class="title">Informations personnelles</div>
+											<div class="container-input">
+												<div class="wrap-input">
+													<div class="input">
+														<input required type="text" placeholder="Prénom*">
+														<div class="error">Error</div>
+													</div>
+												</div>
+												<div class="wrap-input">
+													<div class="input">
+														<input required type="text" placeholder="Prénom*">
+														<div class="error">Error</div>
+													</div>
+												</div>
+												<div class="wrap-input">
+													<div class="input">
+														<input required type="text" placeholder="Prénom*">
+														<div class="error">Error</div>
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="el" data-info="paiement">
+											<div class="title">Informations personnelles</div>
+											<div class="container-input">
+												<div class="wrap-input">
+													<div class="input">
+														<input required type="text" placeholder="Prénom*">
+														<div class="error">Error</div>
+													</div>
+												</div>
+												<div class="wrap-input">
+													<div class="input">
+														<input required type="text" placeholder="Prénom*">
+														<div class="error">Error</div>
+													</div>
+												</div>
+												<div class="wrap-input">
+													<div class="input">
+														<input required type="text" placeholder="Prénom*">
+														<div class="error">Error</div>
+													</div>
+													<div class="input">
+														<input required type="text" placeholder="Prénom*">
+														<div class="error">Error</div>
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="el" data-info="pro">
+											<div class="title">Informations professionnelles</div>
+											<div class="container-input">
+												<div class="wrap-input">
+													<div class="input">
+														<input required type="text" placeholder="Prénom*">
+														<div class="error">Error</div>
+													</div>
+												</div>
+												<div class="wrap-input">
+													<div class="input">
+														<input required type="text" placeholder="Prénom*">
+														<div class="error">Error</div>
+													</div>
+												</div>
+												<div class="wrap-input">
+													<div class="input">
+														<input required type="text" placeholder="Prénom*">
+														<div class="error">Error</div>
+													</div>
+												</div>
+												<div class="wrap-input">
+													<div class="input">
+														<input required type="text" placeholder="Prénom*">
+														<div class="error">Error</div>
+													</div>
+												</div>
+												<div class="wrap-input">
+													<div class="input">
+														<input required type="text" placeholder="Prénom*">
+														<div class="error">Error</div>
+													</div>
+													<div class="input">
+														<input required type="text" placeholder="Prénom*">
+														<div class="error">Error</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</section>
+						<section class="section-bill">
+							<div class="head">
+								<div class="title">Mes Factures</div>
+							</div>
+							<ul>
+								<li>
+									<div class="num">#3142</div>
+									<div class="month">Facture - Janvier</div>
+									<div class="date">12/02/21</div>
+									<a href="" class="link">
+										<span class="link-text">Télécharger</span>
+									</a>
+								</li>
+								<li>
+									<div class="num">#3142</div>
+									<div class="month">Facture - Janvier</div>
+									<div class="date">12/02/21</div>
+									<a href="" class="link">
+										<span class="link-text">Télécharger</span>
+									</a>
+								</li>
+								<li>
+									<div class="num">#3142</div>
+									<div class="month">Facture - Janvier</div>
+									<div class="date">12/02/21</div>
+									<a href="" class="link">
+										<span class="link-text">Télécharger</span>
+									</a>
+								</li>
+								<li>
+									<div class="num">#3142</div>
+									<div class="month">Facture - Janvier</div>
+									<div class="date">12/02/21</div>
+									<a href="" class="link">
+										<span class="link-text">Télécharger</span>
+									</a>
+								</li>
+								<li>
+									<div class="num">#3142</div>
+									<div class="month">Facture - Janvier</div>
+									<div class="date">12/02/21</div>
+									<a href="" class="link">
+										<span class="link-text">Télécharger</span>
+									</a>
+								</li>
+							</ul>
+						</section>
+					</div>
+				</div>
+			</section>
+			
+		</main>
+
+
+		<?php include('common-footer.php') ?>
+
+		<script type="text/javascript" src="script/minify/dashboard-min.js"></script>
+	</body>
+</html>

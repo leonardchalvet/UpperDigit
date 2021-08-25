a29316b47373374c1d435bb83bc1fbf098d920eb
Alexis Prevost-Maynen <alexis@callbruno.com>
2021-08-25 11:23:40 +0200

    Change - Back - Form - Add all fonction for registration

--- public/php/form.php
+++ public/php/form.php
@@ -1,6 +1,8 @@
 <?php 
 
-$id_stripe  = isset($_POST['id_stripe'])   ?  trim($_POST['id_stripe'])  : null ;
+require_once '../../config.php';
+require_once '../../vendor/autoload.php';
+
 $lastname   = isset($_POST['lastname'])    ?  trim($_POST['lastname'])   : null ;
 $firstname  = isset($_POST['firstname'])   ?  trim($_POST['firstname'])  : null ;
 $phone      = isset($_POST['phone'])       ?  trim($_POST['phone'])      : null ;
@@ -17,9 +19,11 @@ $headoffice = isset($_POST['headoffice'])  ?  trim($_POST['headoffice']) : null 
 $pc_adress  = isset($_POST['pc_adress'])   ?  trim($_POST['pc_adress'])  : null ;
 $pc_zipcode = isset($_POST['pc_zipcode'])  ?  trim($_POST['pc_zipcode']) : null ;
 $pc_country = isset($_POST['pc_country'])  ?  trim($_POST['pc_country']) : null ;
+$intent     = isset($_POST['intent'])      ?  trim($_POST['intent'])     : null ;
 
-if( $id_stripe != null
-	&& $lastname != null
+$product = "prod_JxsvEKD9BcS4ak";
+
+if( $lastname != null
 	&& $firstname != null
 	&& $phone != null
 	&& $adress != null
@@ -30,16 +34,134 @@ if( $id_stripe != null
 	&& $cgu != null
 	&& $inform != null ) {
 
-	/*
-	&& $company != null
-	&& $siret != null
-	&& $headoffice != null
-	&& $pc_adress != null
-	&& $pc_zipcode != null
-	&& $pc_country != null */
+	try { 
+		$dbh = new PDO("mysql:dbname=".DB_BASE.";host=".DB_HOST, BD_USER, BD_PASSWORD);
+	} catch ( PDOException $e ) { }
 
-	echo 'true';
+	$taxId = STRIPE_TVA;
 
-	//echo $id_stripe . ' : ' . $lastname . ' : ' . $firstname . ' : ' . $phone . ' : ' . $adress . ' : ' . $zipcode . ' : ' . $country . ' : ' . $mail . ' : ' . $password . ' : ' . $cgu . ' : ' . $inform . ' : ' . $company . ' : ' . $siret . ' : ' . $headoffice . ' : ' . $pc_adress . ' : ' . $pc_zipcode . ' : ' . $pc_country;
+	\Stripe\Stripe::setApiKey(STRIPE);
+
+	$response = \Stripe\Customer::all(["limit" => 1, "email" => $email]);
+
+	if( !empty($response['data']) ) {
+		echo "mail";
+	} else {
+
+		/* ADD IN STIPE */
+		$customer = \Stripe\Customer::create([
+			'name' => $lastname.' '.$firstname,
+			'email' => $mail,
+		  	'phone' => $phone,
+			'address' => [
+				'line1' => $adress,
+				'country' => $country,
+				'postal_code' => $zipcode
+			]
+		]);
+		/* END ADD IN STIPE */
+
+		$user_password = password_hash($password, PASSWORD_DEFAULT);
+
+		/* ADD IN BDD */
+		$query = 'INSERT INTO `customers` (`email`, `password`, `cgu`, `want_news`) VALUES (:email, :password, :cgu, :want_news);';
+		$st = $dbh->prepare($query);
+		$st->execute(array(
+			':email' => $mail,
+			':password' => $user_password,
+			':cgu' => 1,
+			':want_news' => 1,
+		));
+
+		$query = 'SELECT * FROM `customers` WHERE `email` = :email;';
+		$st = $dbh->prepare($query);
+		$st->execute(array(':email' => $mail));
+		$rep = $st->fetch();
+		$id_user = $rep['id'];
+
+		$query = 'INSERT INTO `personal_information` (`id_customers`, `first_name`, `last_name`, `phone`, `adress`, `zip_code`, `country`) VALUES (:id_customers, :first_name, :last_name, :phone, :adress, :zip_code, :country);';
+		$st = $dbh->prepare($query);
+		$st->execute(array(
+			':id_customers' => $id_user,
+			':first_name' => $firstname,
+			':last_name' => $lastname,
+			':phone' => $phone,
+			':adress' => $adress,
+			':zip_code' => $zipcode,
+			':country' => $country
+		));
+
+		if( $company != null
+			&& $siret != null
+			&& $headoffice != null
+			&& $pc_adress != null
+			&& $pc_zipcode != null
+			&& $pc_country != null ) {
+
+			$query = 'INSERT INTO `professional_information` (`id_customers`, `name`, `siret`, `head_office`, `adress`, `zip_code`, `country`) VALUES (:id_customers, :name, :siret, :head_office, :adress, :zip_code, :country);';
+			$st = $dbh->prepare($query);
+			$st->execute(array(
+				':id_customers' => $id_user,
+				':name' => $company,
+				':siret' => $siret,
+				':head_office' => $headoffice,
+				':adress' => $pc_adress,
+				':zip_code' => $pc_zipcode,
+				':country' => $pc_country
+			));
+		}
+
+		/* END ADD IN BDD */
+
+		/* SEND MAIL TO USER */
+		$headers = array(
+		    'From' => $mail,
+		    'Reply-To' => $mail,
+		    'X-Mailer' => 'PHP/' . phpversion()
+		);
+
+		$content =  "Votre compte est actif";
+
+		mail("alexis@callbruno.com", 'Création de compte', $content, $headers);
+		/* END SEND MAIL */
+
+		/* SEND MAIL TO UPPERDIGIT */
+		$headers = array(
+		    'From' => 'Upperdit',
+		    'X-Mailer' => 'PHP/' . phpversion()
+		);
+
+		$content =  "Vous avez un nouvel abonné : ".$mail;
+
+		mail("alexis@callbruno.com", 'Nouvel utilisateur', $content, $headers);
+		/* END SEND MAIL */
+
+		$intent = \Stripe\SetupIntent::retrieve($intent);
+		$payment_method = \Stripe\PaymentMethod::retrieve($intent->payment_method);
+
+		$payment_method->attach(['customer' => $customer['id']]);
+
+		\Stripe\Customer::update(
+		  $customer['id'],
+		  ['invoice_settings' => ['default_payment_method' => $payment_method]]
+		);		
+
+		$subscription = \Stripe\SubscriptionSchedule::create([
+			'customer' => $customer['id'],
+			'start_date' => 'now',
+			'end_behavior' => 'release',
+			'phases' => [
+				[
+					'items' => [
+						[
+							'price' => 'price_1JJx9qFhVHJZbiNtUF0q2iHp',
+							'quantity' => 1,
+						],
+					]
+				],
+			],	
+		]);
+
+	}
 
 }

df11a6acab637c911871ee101205a0defaaefc08
Alexis Prevost-Maynen <alexis@callbruno.com>
2021-08-30 17:52:05 +0200

    Change - Back - Chat - Add chat

--- app/views/common-header.php
+++ app/views/common-header.php
@@ -26,3 +26,10 @@ $header = $WPGLOBAL['header']->data;
 		</div>
 	</div>
 </header>
+
+<script>
+	(function(w,d,u){
+			var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
+			var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+	})(window,document,'https://cdn.bitrix24.fr/b18535555/crm/site_button/loader_2_bdyur2.js');
+</script>

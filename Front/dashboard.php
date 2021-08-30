1b2d2e3f8d1ddcfb77090f6585c6ddbfb105bfaa
Alexis Prevost-Maynen <alexis@callbruno.com>
2021-08-30 17:52:55 +0200

    Change - Front - Dashboard - Add edit & new CTA & interaction

--- Front/dashboard.php
+++ Front/dashboard.php
@@ -11,6 +11,9 @@
 
 		<link rel="stylesheet" type="text/css" href="style/css/dashboard.css">
 
+		<script src="https://js.stripe.com/v3/"></script>
+    	<script src="https://polyfill.io/v3/polyfill.min.js?version=3.52.1&features=fetch"></script>
+
 	</head>
 	
 	<body>
@@ -20,7 +23,59 @@
 
 		<?php include('common-devtools.php') ?>
 
-		
+		<div class="container-lightbox">
+			<form method="post">
+				<input type="email" value="prevost.alexis@hotmail.fr">
+			</form>
+			<div class="wrapper">
+				<div class="lightbox subscribe">
+					<img src="img/dashboard/cross.svg" alt="cross" class="cross">
+					<h3>Êtes-vous sûr de vouloir supprimer votre abonnement ?</h3>
+					<p>Vous êtes sur le point de résilier votre abonnement</p>
+					<div class="container-button">
+						<div class="btn">
+							<div class="btn-text">
+								Annuler
+							</div>
+							<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 16" >
+								<use xlink:href="img/common/icn-arrow.svg#content"></use>
+							</svg>
+						</div>
+						<div class="btn">
+							<div class="btn-text">
+								Résilier votre abonnement
+							</div>
+							<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 16" >
+								<use xlink:href="img/common/icn-arrow.svg#content"></use>
+							</svg>
+						</div>
+					</div>
+				</div>
+				<div class="lightbox account">
+					<img src="img/dashboard/cross.svg" alt="cross" class="cross">
+					<h3>Êtes-vous sûr de vouloir supprimer votre compte ?</h3>
+					<p>Vous êtes sur le point de supprimer définitivement votre compte et cette action est irréversible.</p>
+					<div class="container-button">
+						<div class="btn">
+							<div class="btn-text">
+								Annuler
+							</div>
+							<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 16" >
+								<use xlink:href="img/common/icn-arrow.svg#content"></use>
+							</svg>
+						</div>
+						<div class="btn">
+							<div class="btn-text">
+								Supprimer mon compte
+							</div>
+							<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 16" >
+								<use xlink:href="img/common/icn-arrow.svg#content"></use>
+							</svg>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
 		<main>
 			
@@ -42,7 +97,7 @@
 							<div class="head">
 								<div class="title">Mes informations</div>
 								<div class="container-btn">
-									<div class="btn">
+									<div class="btn style-active">
 										<div class="btn-text">Editer</div>
 									</div>
 									<div class="btn">
@@ -51,7 +106,7 @@
 								</div>
 							</div>
 							<div class="container-content">
-								<div class="content-save">
+								<div class="content-save style-active">
 									<div class="container-el">
 										<div class="el">
 											<ul>
@@ -217,134 +272,141 @@
 								</div>
 								<div class="content-edit">
 									<div class="container-el">
-										<div class="el" data-info="perso">
+										<form class="el" data-info="perso">
+											<input required type="text" value="prevost.alexis@hotmail.fr" name="mail" style="display:none;">
 											<div class="title">Informations personnelles</div>
 											<div class="container-input">
 												<div class="wrap-input">
 													<div class="input">
-														<input required type="text" placeholder="Prénom*">
+														<input required type="text" placeholder="Nom*" name="lastname" value="Prevost-Maynen">
 														<div class="error">Error</div>
 													</div>
 												</div>
 												<div class="wrap-input">
 													<div class="input">
-														<input required type="text" placeholder="Prénom*">
+														<input required type="text" placeholder="Prénom*" name="firstname" value="Alexis">
 														<div class="error">Error</div>
 													</div>
 												</div>
 												<div class="wrap-input">
 													<div class="input">
-														<input required type="text" placeholder="Prénom*">
+														<input required type="text" placeholder="Téléphone*" name="phone" value="0667481456">
 														<div class="error">Error</div>
 													</div>
 												</div>
 												<div class="wrap-input">
 													<div class="input">
-														<input required type="text" placeholder="Prénom*">
+														<input required type="text" placeholder="Adresse postale*" name="adress" value="57 route de dieppe">
 														<div class="error">Error</div>
 													</div>
 												</div>
 												<div class="wrap-input">
 													<div class="input">
-														<input required type="text" placeholder="Prénom*">
+														<input required type="text" placeholder="Code Postal*" name="zipcode" value="76250">
 														<div class="error">Error</div>
 													</div>
 													<div class="input">
-														<input required type="text" placeholder="Prénom*">
+														<input required type="text" placeholder="Pays*" name="country" value="France">
 														<div class="error">Error</div>
 													</div>
 												</div>
 											</div>
-										</div>
-										<div class="el" data-info="securite">
+										</form>
+										<form class="el" data-info="securite">
+											<input required type="text" value="prevost.alexis@hotmail.fr" name="mail" style="display:none;">
 											<div class="title">Informations personnelles</div>
 											<div class="container-input">
 												<div class="wrap-input">
 													<div class="input">
-														<input required type="text" placeholder="Prénom*">
+														<input required type="email" name="email" placeholder="test@test.fr" value="prevost.alexis@hotmail.fr">
 														<div class="error">Error</div>
 													</div>
 												</div>
 												<div class="wrap-input">
 													<div class="input">
-														<input required type="text" placeholder="Prénom*">
+														<input required type="password" name="password1" placeholder="****">
 														<div class="error">Error</div>
 													</div>
 												</div>
 												<div class="wrap-input">
 													<div class="input">
-														<input required type="text" placeholder="Prénom*">
+														<input required type="password" name="password2" placeholder="****">
 														<div class="error">Error</div>
 													</div>
 												</div>
 											</div>
-										</div>
-										<div class="el" data-info="paiement">
+										</form>
+										<form class="el" data-info="paiement">
+											<input required type="text" value="prevost.alexis@hotmail.fr" name="mail" style="display:none;">
+											<input name="intent" type="text" value="0" style="display:none">
 											<div class="title">Informations personnelles</div>
 											<div class="container-input">
 												<div class="wrap-input">
 													<div class="input">
-														<input required type="text" placeholder="Prénom*">
+														<input required type="text" placeholder="Nom de la carte" value="Alexis">
 														<div class="error">Error</div>
 													</div>
 												</div>
-												<div class="wrap-input">
-													<div class="input">
-														<input required type="text" placeholder="Prénom*">
-														<div class="error">Error</div>
+												<div class="input">
+													<div class="wrap-input wrap-input-stripe">
+														<div id="cardNumber-element"></div>
+														<div class="placeholder">**** **** **** ****</div>
 													</div>
 												</div>
-												<div class="wrap-input">
-													<div class="input">
-														<input required type="text" placeholder="Prénom*">
-														<div class="error">Error</div>
+												<div class="input">
+													<div class="wrap-input wrap-input-stripe">
+														<div id="cardExpiry-element"></div>
+														<div class="placeholder">**/**</div>
 													</div>
-													<div class="input">
-														<input required type="text" placeholder="Prénom*">
-														<div class="error">Error</div>
+												</div>
+												<div class="input">
+													<div class="wrap-input wrap-input-stripe">
+														<div id="cardCvc-element"></div>
+														<div class="placeholder">***</div>
 													</div>
 												</div>
 											</div>
-										</div>
-										<div class="el" data-info="pro">
+										</form>
+										<form class="el" data-info="pro">
+											<input required type="text" value="prevost.alexis@hotmail.fr" name="mail" style="display:none;">
 											<div class="title">Informations professionnelles</div>
 											<div class="container-input">
 												<div class="wrap-input">
 													<div class="input">
-														<input required type="text" placeholder="Prénom*">
+														<input required type="text" placeholder="Nom de votre société*" value="bruno">
 														<div class="error">Error</div>
 													</div>
 												</div>
 												<div class="wrap-input">
 													<div class="input">
-														<input required type="text" placeholder="Prénom*">
+														<input required type="text" placeholder="Numéro de SIRET*" value="32132321232">
 														<div class="error">Error</div>
 													</div>
 												</div>
 												<div class="wrap-input">
 													<div class="input">
-														<input required type="text" placeholder="Prénom*">
+														<input required type="text" placeholder="Siège social*" value="BRUNO">
 														<div class="error">Error</div>
 													</div>
 												</div>
 												<div class="wrap-input">
 													<div class="input">
-														<input required type="text" placeholder="Prénom*">
+														<input required type="text" placeholder="Adresse professionnelle*" value="1 rue nicoles oresme">
 														<div class="error">Error</div>
 													</div>
 												</div>
 												<div class="wrap-input">
 													<div class="input">
-														<input required type="text" placeholder="Prénom*">
+														<input required type="text" placeholder="Code Postal*" value="76000">
 														<div class="error">Error</div>
 													</div>
 													<div class="input">
-														<input required type="text" placeholder="Prénom*">
+														<input required type="text" placeholder="Pays*" value="France">
 														<div class="error">Error</div>
 													</div>
 												</div>
 											</div>
-										</div>
+										</form>
 									</div>
 								</div>
 							</div>
@@ -398,6 +460,36 @@
 										<img class="link-icn" src="img/dashboard/icn-download.svg" alt="">
 										<span class="link-text">Télécharger</span>
 									</a>
+								</li>
+							</ul>
+						</section>
+						<section class="section-subscribe">
+							<div class="head">
+								<div class="title">Mon abonnement</div>
+							</div>
+							<ul>
+								<li>
+									<div class="type">Particulier</div>
+									<div class="date">Renouv. le 8 avr 2021</div>
+									<div class="month">Mensuel</div>
+									<div class="price">32,99€</div>
+									<div class="link">
+										<span class="link-text">Résilier mon abonnement</span>
+									</div>
+								</li>
+							</ul>
+						</section>
+						<section class="section-account">
+							<div class="head">
+								<div class="title">Mon compte</div>
+							</div>
+							<ul>
+								<li>
+									<div class="name">Claudia CARVALHO</div>
+									<div class="email">jessica.hanson@example.com</div>
+									<div class="link">
+										<span class="link-text">Supprimer mon compte</span>
+									</div>
 								</li>
 							</ul>
 						</section>

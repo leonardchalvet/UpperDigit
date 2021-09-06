75d628e89166578a1ac66c8beb7ca0a52b8c0713
Alexis Prevost-Maynen <alexis@callbruno.com>
2021-09-06 09:36:35 +0200

    Change - Back - Dashboard - Add all features
    Lightbox - Name - Add form - New sections

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -47,8 +47,14 @@ $professional_information = $st->fetch();
 $stripe = new \Stripe\StripeClient(STRIPE);
 $customer = $stripe->customers->all(['limit' => 1, 'email' => $email])->data[0];
 $invoice = $stripe->invoices->all(['customer' => $customer['id']]);
-$card = $stripe->customers->allSources($customer['id'], ['object' => 'card']);
-print_r( $card );
+$product = $stripe->products->retrieve($invoice['data'][0]['lines']['data'][0]['plan']['product']);
+$upcoming = null;
+try {
+	$upcoming = $stripe->invoices->upcoming(['customer' => $customer['id']]);
+} catch (\Throwable $th) {
+	//throw $th;
+}
+
 ?>
 <html>
   <head>
@@ -65,6 +71,9 @@ print_r( $card );
 
     <link rel="stylesheet" type="text/css" href="/style/css/dashboard.css">
 
+	<script src="https://js.stripe.com/v3/"></script>
+    <script src="https://polyfill.io/v3/polyfill.min.js?version=3.52.1&features=fetch"></script>
+
   </head>
   
   <body>
@@ -74,17 +83,80 @@ print_r( $card );
     <?php include('common-header.php') ?>
 
     <main>
+
+	<div class="container-lightbox">
+		<form method="post">
+			<input type="email" name="email" value="<?php echo($email); ?>">
+		</form>
+		<div class="wrapper">
+			<div class="lightbox subscribe">
+				<img src="/img/dashboard/cross.svg" alt="cross" class="cross">
+				<h3><?= RichText::asText($document->subscribe_lgtbtitle); ?></h3>
+				<p><?= RichText::asText($document->subscribe_lgtbtext); ?></p>
+				<div class="container-button">
+					<div class="btn">
+						<div class="btn-text">
+							<?= RichText::asText($document->subscribe_lgtbbtnno); ?>
+						</div>
+						<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 16" >
+							<use xlink:href="/img/common/icn-arrow.svg#content"></use>
+						</svg>
+					</div>
+					<div class="btn">
+						<div class="btn-text">
+							<?= RichText::asText($document->subscribe_lgtbbtnyes); ?>
+						</div>
+						<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 16" >
+							<use xlink:href="/img/common/icn-arrow.svg#content"></use>
+						</svg>
+					</div>
+				</div>
+			</div>
+			<div class="lightbox account">
+				<img src="/img/dashboard/cross.svg" alt="cross" class="cross">
+				<h3><?= RichText::asText($document->account_lgtbtitle); ?></h3>
+				<p><?= RichText::asText($document->account_lgtbtext); ?></p>
+				<div class="container-button">
+					<div class="btn">
+						<div class="btn-text">
+							<?= RichText::asText($document->account_lgtbbtnno); ?>
+						</div>
+						<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 16" >
+							<use xlink:href="/img/common/icn-arrow.svg#content"></use>
+						</svg>
+					</div>
+					<div class="btn">
+						<div class="btn-text">
+							<?= RichText::asText($document->account_lgtbbtnyes); ?>
+						</div>
+						<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 16" >
+							<use xlink:href="/img/common/icn-arrow.svg#content"></use>
+						</svg>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
       
       <section class="section-dashboard">
+		  	<div id="sectionInformation"></div>
 			<div class="wrapper">
 				<div class="container-sidebar">
 					<div class="title"><?= RichText::asText($document->nav_title); ?></div>
 					<ul class="container-tab">
 						<li class="tab style-active">
-							<span><?= RichText::asText($document->nav_informations); ?></span>
+							<a href="#sectionInformation"><?= RichText::asText($document->nav_informations); ?></a>
 						</li>
 						<li class="tab">
-							<span><?= RichText::asText($document->nav_invoices); ?></span>
+							<a href="#sectionBill"><?= RichText::asText($document->nav_invoices); ?></a>
+						</li>
+						<?php if($upcoming != null) { ?>
+							<li class="tab">
+								<a href="#sectionSubscribe"><?= RichText::asText($document->subscribe_title); ?></a>
+							</li>
+						<?php } ?>
+						<li class="tab">
+							<a href="#sectionAccount"><?= RichText::asText($document->account_title); ?></a>
 						</li>
 					</ul>
 				</div>
@@ -93,7 +165,7 @@ print_r( $card );
 						<div class="head">
 							<div class="title"><?= RichText::asText($document->informations_title); ?></div>
 							<div class="container-btn">
-								<div class="btn">
+								<div class="btn style-active">
 									<div class="btn-text"><?= RichText::asText($document->nav_btntextedit); ?></div>
 								</div>
 								<div class="btn">
@@ -102,7 +174,7 @@ print_r( $card );
 							</div>
 						</div>
 						<div class="container-content">
-							<div class="content-save">
+							<div class="content-save style-active">
 								<div class="container-el">
 									<div class="el">
 										<ul>
@@ -144,18 +216,6 @@ print_r( $card );
 											</li>
 										</ul>
 									</div>
-									<div class="el">
-										<ul>
-											<li>
-												<div class="title"><?= RichText::asText($document->informations_cardholder); ?></div>
-												<div class="text"><?php echo($card->name); ?></div>
-											</li>
-											<li>
-												<div class="title"><?= RichText::asText($document->informations_cardnumber); ?></div>
-												<div class="text"><?php echo($card->brand.' ****  ****  **** '.$card->last4); ?></div>
-											</li>
-										</ul>
-									</div>
 									<?php if( !empty($professional_information) ) { ?>
 									<div class="el">
 										<ul>
@@ -190,97 +250,106 @@ print_r( $card );
 							</div>
 							<div class="content-edit">
 								<div class="container-el">
-									<div class="el" data-info="perso">
+									<form class="el" data-info="perso">
+										<input required type="text" value="<?php echo($email); ?>" name="mail" style="display:none;">
 										<div class="title"><?= RichText::asText($document->informations_title1); ?></div>
 										<div class="container-input">
 											<div class="wrap-input">
 												<div class="input">
-													<input required type="text" value="<?php echo $personal_information['last_name']; ?>">
+													<input required type="text" name="lastname" value="<?php echo $personal_information['last_name']; ?>">
 													<div class="error">Error</div>
 												</div>
 											</div>
 											<div class="wrap-input">
 												<div class="input">
-													<input required type="text" value="<?php echo $personal_information['first_name']; ?>">
+													<input required type="text" name="firstname" value="<?php echo $personal_information['first_name']; ?>">
 													<div class="error">Error</div>
 												</div>
 											</div>
 											<div class="wrap-input">
 												<div class="input">
-													<input required type="text" value="<?php echo $personal_information['phone']; ?>">
+													<input required type="text" name="phone" value="<?php echo $personal_information['phone']; ?>">
 													<div class="error">Error</div>
 												</div>
 											</div>
 											<div class="wrap-input">
 												<div class="input">
-													<input required type="text" value="<?php echo $personal_information['adress']; ?>">
+													<input required type="text" name="adress" value="<?php echo $personal_information['adress']; ?>">
 													<div class="error">Error</div>
 												</div>
 											</div>
 											<div class="wrap-input">
 												<div class="input">
-													<input required type="text" value="<?php echo $personal_information['zip_code']; ?>">
+													<input required type="text" name="zipcode" value="<?php echo $personal_information['zip_code']; ?>">
 													<div class="error">Error</div>
 												</div>
 												<div class="input">
-													<input required type="text" value="<?php echo $personal_information['country']; ?>">
+													<input required type="text" name="country" value="<?php echo $personal_information['country']; ?>">
 													<div class="error">Error</div>
 												</div>
 											</div>
 										</div>
-									</div>
-									<div class="el" data-info="securite">
+									</form>
+									<form class="el" data-info="securite">
 										<div class="title"><?= RichText::asText($document->informations_title2); ?></div>
 										<div class="container-input">
 											<div class="wrap-input">
 												<div class="input">
-													<input required type="text" value="<?php echo($email); ?>">
+													<input required type="text" name="email" value="<?php echo($email); ?>">
 													<div class="error">Error</div>
 												</div>
 											</div>
 											<div class="wrap-input">
 												<div class="input">
-													<input required type="password" placeholder="<?= RichText::asText($document->informations_password); ?>">
+													<input required type="password" name="password1" placeholder="<?= RichText::asText($document->informations_password); ?>">
 													<div class="error">Error</div>
 												</div>
 											</div>
 											<div class="wrap-input">
 												<div class="input">
-													<input required type="password" placeholder="<?= RichText::asText($document->informations_password); ?>">
+													<input required type="password" name="password2" placeholder="<?= RichText::asText($document->informations_password); ?>">
 													<div class="error">Error</div>
 												</div>
 											</div>
 										</div>
-									</div>
-									<div class="el" data-info="paiement">
+									</form>
+									<form class="el" data-info="paiement">
+										<input required type="text" value="<?php echo($email); ?>" name="mail" style="display:none;">
 										<div class="title"><?= RichText::asText($document->informations_title3); ?></div>
 										<div class="container-input">
 											<div class="wrap-input">
 												<div class="input">
-													<input required type="text" value="<?php echo $personal_information['last_name']; ?>">
+													<input required type="text" name="name" value="<?php echo $personal_information['last_name']; ?>">
 													<div class="error">Error</div>
 												</div>
 											</div>
 											<div class="wrap-input">
 												<div class="input">
-													<input required type="text" value="<?php echo ""; ?>">
-													<div class="error">Error</div>
+													<div class="wrap-input wrap-input-stripe">
+														<div id="cardNumber-element"></div>
+														<div class="placeholder">**** **** **** ****</div>
+													</div>
 												</div>
 											</div>
 											<div class="wrap-input">
 												<div class="input">
-													<input required type="text" value="<?php echo ""; ?>">
-													<div class="error">Error</div>
+													<div class="wrap-input wrap-input-stripe">
+														<div id="cardExpiry-element"></div>
+														<div class="placeholder">**/**</div>
+													</div>
 												</div>
 												<div class="input">
-													<input required type="text" value="<?php echo ""; ?>">
-													<div class="error">Error</div>
+													<div class="wrap-input wrap-input-stripe">
+														<div id="cardCvc-element"></div>
+														<div class="placeholder">***</div>
+													</div>
 												</div>
 											</div>
 										</div>
-									</div>
+									</form>
 									<?php if( !empty($professional_information) ) { ?>
-									<div class="el" data-info="pro">
+									<form class="el" data-info="pro">
+										<input required type="text" value="<?php echo($email); ?>" name="mail" style="display:none;">
 										<div class="title"><?= RichText::asText($document->informations_title4); ?></div>
 										<div class="container-input">
 											<div class="wrap-input">
@@ -318,10 +387,11 @@ print_r( $card );
 												</div>
 											</div>
 										</div>
-									</div>
+									</form>
 									<?php } ?>
 								</div>
 							</div>
+							<div id="sectionBill"></div>
 						</div>
 					</section>
 					<section class="section-bill">
@@ -334,14 +404,56 @@ print_r( $card );
 							$date->setTimestamp($i->created);
 							?>
 							<li>
-								<div class="num"><?php echo($i->number); ?></div>
-								<div class="month"><?php echo(monthInFrench(date_format($date, 'm'))) ?></div>
-								<div class="date"><?php echo(date_format($date, 'd/m/Y')); ?></div>
-								<a href="<?php echo($i->hosted_invoice_url); ?>" class="link" target="_blank">
-									<span class="link-text"><?= RichText::asText($document->invoices_download); ?></span>
-								</a>
+								<?php if(empty($i->number)){ ?>
+									<div class="num">La fature sera disponible sous maximum 24h</div>
+								<?php } else { ?>
+									<div class="num"><?php echo($i->number); ?></div>
+									<div class="month"><?php echo(monthInFrench(date_format($date, 'm'))) ?></div>
+									<div class="date"><?php echo(date_format($date, 'd/m/Y')); ?></div>
+									<a href="<?php echo($i->hosted_invoice_url); ?>" class="link" target="_blank">
+										<span class="link-text"><?= RichText::asText($document->invoices_download); ?></span>
+									</a>
+								<?php } ?>
 							</li>
 							<?php } ?>
+						</ul>
+						<?php if($upcoming != null) { ?>
+							<div id="sectionSubscribe"></div>
+						<?php } else { ?>
+							<div id="sectionAccount"></div>
+						<?php } ?>
+					</section>
+					<?php if($upcoming != null) { ?>
+					<section class="section-subscribe">
+						<div class="head">
+							<div class="title"><?= RichText::asText($document->subscribe_title); ?></div>
+						</div>
+						<ul>
+							<li>
+								<div class="type"><?php echo($product['name']); ?></div>
+								<div class="date"><?= RichText::asText($document->subscribe_updated); ?> <?php echo(date('d/m/Y', $upcoming['next_payment_attempt'])); ?></div>
+								<div class="month"><?= RichText::asText($document->subscribe_month); ?></div>
+								<div class="price"><?php echo( ''. ( intval($invoice['data'][0]['lines']['data'][0]['plan']['amount']) / 100) .'€'); ?></div>
+								<div class="link">
+									<span class="link-text"><?= RichText::asText($document->subscribe_cancel); ?></span>
+								</div>
+							</li>
+						</ul>
+						<div id="sectionAccount"></div>
+					</section>
+					<?php } ?>
+					<section class="section-account">
+						<div class="head">
+							<div class="title"><?= RichText::asText($document->account_title); ?></div>
+						</div>
+						<ul>
+							<li>
+								<div class="name"><?php echo($personal_information['first_name']); ?> <?php echo(strtoupper($personal_information['last_name'])); ?></div>
+								<div class="email"><?php echo($email); ?></div>
+								<div class="link">
+									<span class="link-text"><?= RichText::asText($document->account_cancel); ?></span>
+								</div>
+							</li>
 						</ul>
 					</section>
 				</div>

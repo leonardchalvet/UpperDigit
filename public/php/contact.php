b8a326af916a833fa544190362e05d1c222e36ac
Alexis Prevost-Maynen <alexis@callbruno.com>
2021-09-06 17:25:53 +0200

    Change - back - Contact - Send mail

--- public/php/contact.php
+++ public/php/contact.php
@@ -1,3 +1,25 @@
 <?php
 
-echo 'true';
+require_once '../../config.php';
+
+$name    = isset($_POST['name'])    ? trim($_POST['name'])    : null ;
+$mail    = isset($_POST['email'])   ? trim($_POST['email'])   : null ;
+$message = isset($_POST['message']) ? trim($_POST['message']) : null ;
+
+if( $name != null 
+    && $mail != null
+    && $message != null ) {
+
+    $headers = array(
+        'From' => $mail,
+        'Reply-To' => $mail,
+        'X-Mailer' => 'PHP/' . phpversion()
+    );
+
+    $content =  $message;
+    
+    mail($mail, 'Contact', $content, $headers);
+
+    echo 'true';
+
+}
